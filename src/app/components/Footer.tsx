848325c09aabefb1fb794e2c4ca1b3830e329595
stephen james <jeratdevworks@gmail.com>
2025-05-09 22:48:36 +0100

    adding slider, product and categories

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="">
+      {/* Top  */}
+      <div className="">
+        {/* LEFT  */}
+        <div className="w-1/4"></div>
+        {/* CENTER */}
+        <div className="w-1/2"></div>
+        {/* RIGHT */}
+        <div className="w-1/4"></div>
+      </div>
+      {/* BOTTON */}
+      <div className=""></div>
+    </div>
+  );
 };
 
 export default Footer;
