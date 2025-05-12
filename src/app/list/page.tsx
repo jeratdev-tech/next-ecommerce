f042bfc3bb5017eff3547a61d97f85c0229b6517
stephen james <jeratdevworks@gmail.com>
2025-05-12 01:36:49 +0100

    error fix 11

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,19 +1,11 @@
 "use client";
 
-// import Filter from "@/components/Filter";
 // import ProductList from "@/components/ProductList";
 // import Skeleton from "@/components/Skeleton";
 // import { wixClientServer } from "@/lib/wixClientServer";
 import Image from "next/image";
 import { Suspense } from "react";
 import Filter from "../components/Filter";
-
-// const ListPage = async ({ searchParams }: { searchParams: any }) => {
-//   const wixClient = await wixClientServer();
-
-//   const cat = await wixClient.collections.getCollectionBySlug(
-//     searchParams.cat || "all-products"
-//   );
 
 const ListPage = () => {
   return (
@@ -36,17 +28,6 @@ const ListPage = () => {
       {/* FILTER */}
       <Filter />
       {/* PRODUCTS */}
-      {/* <h1 className="mt-12 text-xl font-semibold">
-        {cat?.collection?.name} For You!
-      </h1>
-      <Suspense fallback={<Skeleton />}>
-        <ProductList
-          categoryId={
-            cat.collection?._id || "00000000-000000-000000-000000000001"
-          }
-          searchParams={searchParams}
-        />
-      </Suspense> */}
     </div>
   );
 };
